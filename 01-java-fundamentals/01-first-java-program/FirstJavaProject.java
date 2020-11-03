2db94fb87073a86439d0ff4b3845da465a43d7c0
Thomas Krueger <thomas.krueger1972@gmail.com>
2020-11-03 15:12:29 -0500

    first project

---
+++ 01-java-fundamentals/01-first-java-program/FirstJavaProject.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+
+public class FirstJavaProject {
+    public static void main(String[] args) {
+        System.out.println("My Name is Thomas Krueger");
+        System.out.println("I am 47 years old");
+        System.out.println("My hometown is Woodbridge, VA");
+    }
+}
+	
